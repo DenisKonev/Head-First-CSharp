445690f4eeaf1c9297754c29a1c7461a214bdae3
DenisKonev <deniskonev1984@gmail.com>
2020-08-18 22:21:26 +0200

    Deck of Cards solution added
    Deck of Cards solution added
    p. 355

---
+++ DeckOfCards/DeckOfCards/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            do
+            {
+                int numberBeetwen0and3 = random.Next(4);
+                int numberBeetwen1and13 = random.Next(1, 14);
+                Card card = new Card((Suits)numberBeetwen0and3, (Values)numberBeetwen1and13);
+                Console.WriteLine(card.Name);
+                Thread.Sleep(1000);
+            } while (true);
+        }
+    }
+}
