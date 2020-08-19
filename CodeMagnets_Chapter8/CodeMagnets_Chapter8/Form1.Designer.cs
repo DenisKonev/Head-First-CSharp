aea50dcfdbfb6031f2384281a2546365cff4a7b6
DenisKonev <deniskonev1984@gmail.com>
2020-08-19 18:48:36 +0200

    Code Magnets Chapter 8 solution added

---
+++ CodeMagnets_Chapter8/CodeMagnets_Chapter8/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CodeMagnets_Chapter8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.AutoSize = true;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(78, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(215, 86);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Press me!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(400, 300);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Code Magnets";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CodeMagnets_Chapter8/CodeMagnets_Chapter8/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeMagnets_Chapter8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void printL(List<string> a)
+        {
+            string result = "";
+            foreach (string element in a)
+                result += "\n" + element;
+            MessageBox.Show(result);
+        }
+
+            private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> a = new List<string>();
+            string zilch = "zero";
+            string first = "one";
+            string second = "two";
+            string third = "three";
+            string fourth = "4.2";
+            string twopointtwo = "2.2";
+
+            a.Add(zilch);
+            a.Add(first);
+            a.Add(second);
+            a.Add(third);
+
+            a.RemoveAt(2);
+
+            if (a.Contains("two"))
+            {
+                a.Add(twopointtwo);
+            }
+
+            if (a.Contains("three"))
+            {
+                a.Add("four");
+            }
+
+            if (a.IndexOf("four") != 4)
+            {
+                a.Add(fourth);
+            }
+            printL(a);
+        }
+    }
+}
