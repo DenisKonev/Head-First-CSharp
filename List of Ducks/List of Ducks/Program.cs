d8d217f53462eb916aa2e3c180a0e20d4fac28d4
DenisKonev <deniskonev1984@gmail.com>
2020-08-20 20:13:21 +0200

    List of Ducks solution added

---
+++ List of Ducks/List of Ducks/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_of_Ducks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Duck> ducks = new List<Duck>() 
+            {
+            new Duck() { Kind = KindOfDuck.Mallard, Size = 17 },
+            new Duck() { Kind = KindOfDuck.Muscovy, Size = 18 },
+            new Duck() { Kind = KindOfDuck.Decoy, Size = 14 },
+            new Duck() { Kind = KindOfDuck.Muscovy, Size = 11 },
+            new Duck() { Kind = KindOfDuck.Mallard, Size = 14 },
+            new Duck() { Kind = KindOfDuck.Decoy, Size = 13 },
+            };
+
+            DuckComparer comparer = new DuckComparer();
+
+            comparer.SortBy = SortCriteria.KindThenSize;
+            ducks.Sort(comparer);
+            PrintDucks(ducks);
+            
+            comparer.SortBy = SortCriteria.SizeThenKind;
+            ducks.Sort(comparer);
+            PrintDucks(ducks);
+        }
+        public static void PrintDucks(List<Duck> ducks)
+        {
+            foreach (Duck duck in ducks)
+                Console.WriteLine(duck.Size.ToString() + "-inch " + duck.Kind.ToString());
+            Console.WriteLine("End of ducks!");
+            Console.ReadKey();
+        }
+    }
+}
