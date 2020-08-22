e230e897368d25db3099200f8d378e7bc594709b
DenisKonev <deniskonev1984@gmail.com>
2020-08-22 19:09:00 +0200

    Two solutions added
    1) List of Ducks
    2) List of Cards

--- List of Ducks/List of Ducks/Program.cs
+++ List of Ducks/List of Ducks/Program.cs
@@ -20,14 +20,25 @@ namespace List_of_Ducks
             new Duck() { Kind = KindOfDuck.Decoy, Size = 13 },
             };
 
-            DuckComparer comparer = new DuckComparer();
+            //Sorting by size with overridden CompareTo() in Duck class
+            ducks.Sort();
+            PrintDucks(ducks);
 
-            comparer.SortBy = SortCriteria.KindThenSize;
+            DuckComparerBySize comparer = new DuckComparerBySize();
+
+            //Sorting by size using IComparer<Duck>
             ducks.Sort(comparer);
             PrintDucks(ducks);
-            
-            comparer.SortBy = SortCriteria.SizeThenKind;
-            ducks.Sort(comparer);
+
+            //Sorting by size or kind depending on SortBy field
+            DuckComparer comparerWithOptrions = new DuckComparer();
+
+            comparerWithOptrions.SortBy = SortCriteria.KindThenSize;
+            ducks.Sort(comparerWithOptrions);
+            PrintDucks(ducks);
+
+            comparerWithOptrions.SortBy = SortCriteria.SizeThenKind;
+            ducks.Sort(comparerWithOptrions);
             PrintDucks(ducks);
         }
         public static void PrintDucks(List<Duck> ducks)
