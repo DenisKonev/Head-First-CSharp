46d86f64baaa42043c188194903bcdc8512c5d2a
DenisKonev <deniskonev1984@gmail.com>
2020-08-23 18:52:31 +0200

    Retired Jersey Numbers solution added

---
+++ Retired Jersey Numbers/Retired Jersey Numbers/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Retired_Jersey_Numbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.number = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.yearLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "was worn by";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.nameLabel.Location = new System.Drawing.Point(345, 27);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(194, 27);
+            this.nameLabel.TabIndex = 2;
+            // 
+            // number
+            // 
+            this.number.FormattingEnabled = true;
+            this.number.Location = new System.Drawing.Point(144, 24);
+            this.number.Name = "number";
+            this.number.Size = new System.Drawing.Size(49, 33);
+            this.number.TabIndex = 3;
+            this.number.SelectedIndexChanged += new System.EventHandler(this.number_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(545, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "and retired in";
+            // 
+            // yearLabel
+            // 
+            this.yearLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.yearLabel.Location = new System.Drawing.Point(708, 28);
+            this.yearLabel.Name = "yearLabel";
+            this.yearLabel.Size = new System.Drawing.Size(70, 25);
+            this.yearLabel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 78);
+            this.Controls.Add(this.yearLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.number);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Retired Jersey Numbers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.ComboBox number;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label yearLabel;
+    }
+}
+
