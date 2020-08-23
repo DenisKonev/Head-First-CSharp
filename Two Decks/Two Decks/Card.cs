ade6a4249cfd6d8a7f71e3f63db7caaf4ae3f20e
DenisKonev <deniskonev1984@gmail.com>
2020-08-23 16:12:16 +0200

    Two Decks solution added

---
+++ Two Decks/Two Decks/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Decks
+{
+    class Card
+    {
+        public Suits Suit { get; set; }
+        public Values Value { get; set; }
+        public Card(Suits suit, Values value)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+        public string Name { get { return Value.ToString() + " of " + Suit.ToString(); } }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Two Decks/Two Decks/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Two_Decks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reset1 = new System.Windows.Forms.Button();
+            this.shuffle1 = new System.Windows.Forms.Button();
+            this.reset2 = new System.Windows.Forms.Button();
+            this.shuffle2 = new System.Windows.Forms.Button();
+            this.moveToDeck2 = new System.Windows.Forms.Button();
+            this.moveToDeck1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // reset1
+            // 
+            this.reset1.Location = new System.Drawing.Point(15, 511);
+            this.reset1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.reset1.Name = "reset1";
+            this.reset1.Size = new System.Drawing.Size(200, 44);
+            this.reset1.TabIndex = 0;
+            this.reset1.Text = "Reset Deck #1";
+            this.reset1.UseVisualStyleBackColor = true;
+            this.reset1.Click += new System.EventHandler(this.reset1_Click);
+            // 
+            // shuffle1
+            // 
+            this.shuffle1.Location = new System.Drawing.Point(15, 577);
+            this.shuffle1.Margin = new System.Windows.Forms.Padding(6);
+            this.shuffle1.Name = "shuffle1";
+            this.shuffle1.Size = new System.Drawing.Size(200, 44);
+            this.shuffle1.TabIndex = 1;
+            this.shuffle1.Text = "Shuffle Deck #1";
+            this.shuffle1.UseVisualStyleBackColor = true;
+            this.shuffle1.Click += new System.EventHandler(this.shuffle1_Click);
+            // 
+            // reset2
+            // 
+            this.reset2.Location = new System.Drawing.Point(357, 511);
+            this.reset2.Margin = new System.Windows.Forms.Padding(6);
+            this.reset2.Name = "reset2";
+            this.reset2.Size = new System.Drawing.Size(200, 44);
+            this.reset2.TabIndex = 2;
+            this.reset2.Text = "Reset Deck #2";
+            this.reset2.UseVisualStyleBackColor = true;
+            this.reset2.Click += new System.EventHandler(this.reset2_Click);
+            // 
+            // shuffle2
+            // 
+            this.shuffle2.Location = new System.Drawing.Point(357, 577);
+            this.shuffle2.Margin = new System.Windows.Forms.Padding(6);
+            this.shuffle2.Name = "shuffle2";
+            this.shuffle2.Size = new System.Drawing.Size(200, 44);
+            this.shuffle2.TabIndex = 3;
+            this.shuffle2.Text = "Shuffle Deck #2";
+            this.shuffle2.UseVisualStyleBackColor = true;
+            this.shuffle2.Click += new System.EventHandler(this.shuffle2_Click);
+            // 
+            // moveToDeck2
+            // 
+            this.moveToDeck2.Location = new System.Drawing.Point(247, 149);
+            this.moveToDeck2.Margin = new System.Windows.Forms.Padding(6);
+            this.moveToDeck2.Name = "moveToDeck2";
+            this.moveToDeck2.Size = new System.Drawing.Size(79, 44);
+            this.moveToDeck2.TabIndex = 4;
+            this.moveToDeck2.Text = ">>";
+            this.moveToDeck2.UseVisualStyleBackColor = true;
+            this.moveToDeck2.Click += new System.EventHandler(this.moveToDeck2_Click);
+            // 
+            // moveToDeck1
+            // 
+            this.moveToDeck1.Location = new System.Drawing.Point(247, 243);
+            this.moveToDeck1.Margin = new System.Windows.Forms.Padding(6);
+            this.moveToDeck1.Name = "moveToDeck1";
+            this.moveToDeck1.Size = new System.Drawing.Size(79, 44);
+            this.moveToDeck1.TabIndex = 5;
+            this.moveToDeck1.Text = "<<";
+            this.moveToDeck1.UseVisualStyleBackColor = true;
+            this.moveToDeck1.Click += new System.EventHandler(this.moveToDeck1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Deck #1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(352, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Deck #2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(15, 49);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(200, 429);
+            this.listBox1.TabIndex = 10;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.HorizontalScrollbar = true;
+            this.listBox2.ItemHeight = 25;
+            this.listBox2.Location = new System.Drawing.Point(357, 49);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(200, 429);
+            this.listBox2.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 644);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.moveToDeck1);
+            this.Controls.Add(this.moveToDeck2);
+            this.Controls.Add(this.shuffle2);
+            this.Controls.Add(this.reset2);
+            this.Controls.Add(this.shuffle1);
+            this.Controls.Add(this.reset1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(588, 683);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(588, 683);
+            this.Name = "Form1";
+            this.Text = "Two Decks";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button reset1;
+        private System.Windows.Forms.Button shuffle1;
+        private System.Windows.Forms.Button reset2;
+        private System.Windows.Forms.Button shuffle2;
+        private System.Windows.Forms.Button moveToDeck2;
+        private System.Windows.Forms.Button moveToDeck1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
