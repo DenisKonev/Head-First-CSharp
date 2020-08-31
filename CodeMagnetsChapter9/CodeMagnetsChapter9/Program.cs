427ded05a0cc9b1ec13625958cc864ed337683eb
DenisKonev <deniskonev1984@gmail.com>
2020-08-31 21:59:08 +0200

    PoolPuzzleChapter7 solution added

---
+++ CodeMagnetsChapter9/CodeMagnetsChapter9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeMagnetsChapter9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Flobbo f = new Flobbo("blue yellow");
+            StreamWriter sw = f.Snobbo();
+            f.Blobbo(f.Blobbo(f.Blobbo(sw), sw), sw);
+        }
+    }
+}
