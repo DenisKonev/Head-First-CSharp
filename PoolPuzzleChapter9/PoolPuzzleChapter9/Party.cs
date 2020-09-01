8a71b75bf7f6c1fb4232897ba4d7b2df95fa1926
DenisKonev <deniskonev1984@gmail.com>
2020-09-01 20:19:43 +0200

    PoolPuzzleChapter9 solution added

---
+++ PoolPuzzleChapter9/PoolPuzzleChapter9/Party.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoolPuzzleChapter9
+{
+    class Party
+    {
+        private StreamReader reader;
+        public Party(StreamReader reader)
+        {
+            this.reader = reader;
+        }
+        public void HowMuch(StreamWriter q)
+        {
+            q.WriteLine(reader.ReadLine());
+            reader.Close();
+        }
+    }
+}
---
+++ PoolPuzzleChapter9/PoolPuzzleChapter9/Pizza.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoolPuzzleChapter9
+{
+    class Pizza
+    {
+        private StreamWriter writer;
+        public Pizza(StreamWriter writer)
+        {
+            this.writer = writer;
+        }
+        public void Idaho(Pineapple.Fargo f)
+        {
+            writer.WriteLine(f);
+            writer.Close();
+        }
+    }
+}
