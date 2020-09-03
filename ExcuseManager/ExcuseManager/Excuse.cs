80933e999e803868037306edea0d6b5941657e97
DenisKonev <deniskonev1984@gmail.com>
2020-09-03 19:04:29 +0200

    ExcuseManager solution added

---
+++ ExcuseManager/ExcuseManager/Excuse.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcuseManager
+{
+    class Excuse
+    {
+        public string Description { get; set; }
+        public string Results { get; set; }
+        public DateTime LastUsed { get; set; }
+        public string ExcusePath { get; set; }
+        public Excuse() { ExcusePath = ""; }
+        public Excuse(string excusePath) { OpenFile(excusePath); }
+        public Excuse(Random random, string folder) 
+        {
+            string[] fileNames = Directory.GetFiles(folder, "*.txt");
+            OpenFile(fileNames[random.Next(fileNames.Length)]);
+        }
+        public void OpenFile(string excusePath)
+        {
+            this.ExcusePath = excusePath;
+            using (StreamReader reader = new StreamReader(excusePath))
+            {
+                Description = reader.ReadLine();
+                Results = reader.ReadLine();
+                LastUsed = Convert.ToDateTime(reader.ReadLine());
+            }
+        }
+        public void Save(string fileName) 
+        {
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                writer.WriteLine(Description);
+                writer.WriteLine(Results);
+                writer.WriteLine(LastUsed);
+            }
+        }
+    }
+}
---
+++ ExcuseManager/ExcuseManager/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcuseManager
+{
+    public partial class Form1 : Form
+    {
+        bool formChanged;
+        string selectedFolder;
+        Excuse currentExcuse;
+        Random random;
+
+        public Form1()
+        {
+            InitializeComponent();
+            selectedFolder = @"C:\Temp\Excuse manager";
+            random = new Random();
+            currentExcuse = new Excuse();
+            currentExcuse.LastUsed = lastUsed.Value;
+            UpdateForm(formChanged);
+        }
+
+        private void UpdateForm(bool changed)
+        {
+            if (!changed)
+            {
+                this.description.Text = currentExcuse.Description;
+                this.results.Text = currentExcuse.Results;
+                this.lastUsed.Value = currentExcuse.LastUsed;
+                if (!String.IsNullOrEmpty(currentExcuse.ExcusePath))
+                    fileDate.Text = File.GetLastWriteTime(currentExcuse.ExcusePath).ToString();
+                this.Text = "Excuse Manager";
+            }
+            else
+                this.Text = "Excuse Manager*";
+            this.formChanged = changed;
+        }
+
+        private void folderButton_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = selectedFolder;
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                selectedFolder = folderBrowserDialog1.SelectedPath;
+                saveButton.Enabled = true;
+                openButton.Enabled = true;
+                randomButton.Enabled = true;
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(description.Text) || String.IsNullOrEmpty(results.Text))
+            {
+                MessageBox.Show("Please specify an excuse and a result", "Unable to save", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            saveFileDialog1.InitialDirectory = selectedFolder;
+            saveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FileName = description.Text + ".txt";
+            DialogResult result = saveFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                currentExcuse.Save(saveFileDialog1.FileName);
+                UpdateForm(false);
+                MessageBox.Show("Excuse written");
+            }
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            if (CheckChanged())
+            {
+                openFileDialog1.InitialDirectory = selectedFolder;
+                openFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog1.FileName = description.Text + ".txt";
+                DialogResult result = openFileDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    currentExcuse = new Excuse(openFileDialog1.FileName);
+                    UpdateForm(false);
+                }
+            }
+        }
+        private void randomButton_Click(object sender, EventArgs e)
+        {
+            if (CheckChanged())
+            {
+                currentExcuse = new Excuse(random, selectedFolder);
+                UpdateForm(false);
+            }
+        }
+        private bool CheckChanged()
+        {
+            if (formChanged)
+            {
+                DialogResult result = MessageBox.Show("The current excuse has not been saved. Continue?",
+                    "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.No)
+                    return false;
+            }
+            return true;
+        }
+
+        private void description_TextChanged(object sender, EventArgs e)
+        {
+            currentExcuse.Description = description.Text;
+            UpdateForm(true);
+        }
+
+        private void results_TextChanged(object sender, EventArgs e)
+        {
+            currentExcuse.Results = results.Text;
+            UpdateForm(true);
+        }
+
+        private void lastUsed_ValueChanged(object sender, EventArgs e)
+        {
+            currentExcuse.LastUsed = lastUsed.Value;
+            UpdateForm(true);
+        }
+    }
+}
