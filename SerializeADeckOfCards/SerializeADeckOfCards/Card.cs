a0921ed1d47656b0d69afac0a7f42fd8885130b7
DenisKonev <deniskonev1984@gmail.com>
2020-09-12 19:54:30 +0200

    SerializeADeckOfCards solution added

---
+++ SerializeADeckOfCards/SerializeADeckOfCards/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeADeckOfCards
+{
+    [Serializable]
+    class Card
+    {
+        public Suits Suit { get; set; }
+        public Values Value { get; set; }
+        public Card(Suits suit, Values value)
+        {
+            this.Suit = suit;
+            this.Value = value;
+        }
+        public string Name { get { return Value.ToString() + " of " + Suit.ToString(); } }
+    }
+}
