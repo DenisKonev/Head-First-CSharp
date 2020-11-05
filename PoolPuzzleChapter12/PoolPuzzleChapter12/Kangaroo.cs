225db573a9b9c4c935b3f77427fea827d8fb2c0c
DenisKonev <deniskonev1984@gmail.com>
2020-11-05 22:15:29 +0100

    PoolPuzzleChapter12 initial upload

---
+++ PoolPuzzleChapter12/PoolPuzzleChapter12/Kangaroo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoolPuzzleChapter12
+{
+    class Kangaroo
+    {
+        FileStream fs;
+        int croc;
+        int dingo = 0;
+        public int Wombat(int wallaby)
+        {
+            _______ __;
+            try
+            {
+                if (wallaby > 0)
+                {
+                    fs = fs.OpenWrite("wobbiegong");
+                    croc = 0;
+                }
+                else if (wallaby < 0)
+                {
+                    croc = 3;
+                }
+                else
+                {
+                    ___ = _____.OpenRead("wobbiegong");
+                    croc = 1;
+                }
+            }
+            catch (IOException)
+            {
+                croc = -3;
+            }
+            catch
+            {
+                croc = 4;
+            }
+            finally
+            {
+                if (______ > 2)
+                {
+                    croc ___ dingo;
+                }
+            }
+            return ______;
+        }
+}
---
+++ PoolPuzzleChapter12/PoolPuzzleChapter12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoolPuzzleChapter12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kangaroo joey = new Kangaroo();
+            int koala = joey.Wombat(joey.Wombat(joey.Wombat(1)));
+            try
+            {
+                Console.WriteLine((15 / koala) + " eggs per pound");
+            }
+            catch (___________________)
+            {
+                Console.WriteLine("G’Day Mate!");
+            }
+        }
+    }
+}
