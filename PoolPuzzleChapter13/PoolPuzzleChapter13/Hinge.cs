abd1daa256e0e95077de4ca88d130a3c557efd0b
DenisKonev <deniskonev1984@gmail.com>
2020-11-12 17:32:05 +0100

    PoolPuzzleChapter13 solution added

---
+++ PoolPuzzleChapter13/PoolPuzzleChapter13/Hinge.cs
@@ -0,0 +1,16 @@
+﻿namespace PoolPuzzleChapter13
+{
+    public class Hinge
+    {
+        public int bulb;
+        public Table garden;
+        public void Set(Table a)
+        {
+            garden = a;
+        }
+        public string Table()
+        {
+            return garden.stairs;
+        }
+    }
+}
---
+++ PoolPuzzleChapter13/PoolPuzzleChapter13/Table.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoolPuzzleChapter13
+{
+    public struct Table
+    {
+        public string stairs;
+        public Hinge floor;
+        public void Set(Hinge b)
+        {
+            floor = b;
+        }
+        public void Lamp(object oil)
+        {
+            if (oil is int)
+                floor.bulb = (int)oil;
+            else if (oil is string)
+                stairs = (string)oil;
+            else if (oil is Hinge) 
+            {
+                Hinge vine = oil as Hinge;
+                Console.WriteLine(vine.Table() + " " + vine.bulb + " " + stairs);
+                Console.ReadKey();
+            }
+        }
+    }
+}
