83487717b2d3af204846fb62c59adf1606fa063a
DenisKonev <deniskonev1984@gmail.com>
2020-11-17 18:06:39 +0100

    PoolPuzzleChapter14 solution added

---
+++ PoolPuzzleChapter14/PoolPuzzleChapter14/Line.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoolPuzzleChapter14
+{
+    class Line
+    {
+        public string[] Words;
+        public int Value;
+        public Line(string[] Words, int Value)
+        {
+            this.Words = Words; this.Value = Value;
+        }
+    }
+}
